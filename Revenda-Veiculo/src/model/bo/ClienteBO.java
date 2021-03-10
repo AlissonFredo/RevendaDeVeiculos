9981ae9d7dd0de751f836fc891ad63b92bec1740
alisson fredo <douglasalissonsf@gmail.com>
2021-03-10 01:39:00 -0300

    revendaVeiculo - add class ClienteBO

--- Revenda-Veiculo/src/model/bo/ClienteBO.java
+++ Revenda-Veiculo/src/model/bo/ClienteBO.java
@@ -2,33 +2,70 @@ package model.bo;
 
 import java.util.ArrayList;
 
+import model.dao.ClienteDAO;
 import model.vo.ClienteVO;
 
 public class ClienteBO {
 
 	public String cadastrarClienteBO(ClienteVO clienteVO) {
-		// TODO Auto-generated method stub
-		return null;
+		String retorno;
+		ClienteDAO clienteDAO = new ClienteDAO();
+
+		if (clienteDAO.verficarCliente(clienteVO.getCpf())) {
+			retorno = "\n CLIENTE JÁ CADASTRADO!";
+		} else {
+			int resultado = clienteDAO.cadastrarClienteDAO(clienteVO);
+			if (resultado == 1) {
+				retorno = "\n CLIENTE CADASTRADO COM SUCESSO!";
+			} else {
+				retorno = "\n NÃO FOI POSSIVEL CADASTRAR O CLIENTE!";
+			}
+		}
+		return retorno;
 	}
 
 	public String atualizarClienteBO(ClienteVO clienteVO) {
-		// TODO Auto-generated method stub
-		return null;
+		String retorno;
+		ClienteDAO clienteDAO = new ClienteDAO();
+
+		if (clienteDAO.verificarClientePorId(clienteVO.getIdCliente())) {
+			int resultado = clienteDAO.atualizarClienteDAO(clienteVO);
+			if (resultado == 1) {
+				retorno = "\n CLIENTE ATUALIZADO COM SUCESSO";
+			} else {
+				retorno = "\n NÃO FOI POSSÍVEL ATUALIZAR CLIENTE!";
+			}
+		} else {
+			retorno = "\n CLIENTE NÃO ESTÁ REGISTRADO NO BANCO!";
+		}
+		return retorno;
 	}
 
 	public String excluirClienteBO(ClienteVO clienteVO) {
-		// TODO Auto-generated method stub
-		return null;
+		String retorno;
+		ClienteDAO clienteDAO = new ClienteDAO();
+
+		if (clienteDAO.verificarClientePorId(clienteVO.getIdCliente())) {
+			int resultado = clienteDAO.excluirClienteDAO(clienteVO);
+			if (resultado == 1) {
+				retorno = "\n CLIENTE EXCLUIDO COM SUCESSO!";
+			} else {
+				retorno = "\n NÃO FOI POSSIVEL EXCLUIR CLIENTE!";
+			}
+		} else {
+			retorno = "\n CLIENTE NÃO ENCONTRADO NO BANCO!";
+		}
+		return retorno;
 	}
 
 	public ClienteVO consultarUmClienteBO(ClienteVO clienteVO) {
-		// TODO Auto-generated method stub
-		return null;
+		ClienteDAO clienteDAO = new ClienteDAO();
+		return clienteDAO.consultarUmClienteDAO(clienteVO);
 	}
 
 	public ArrayList<ClienteVO> consultarTodosClientesBO() {
-		// TODO Auto-generated method stub
-		return null;
+		ClienteDAO clienteDAO = new ClienteDAO();
+		return clienteDAO.consultarTodosClientesDAO();
 	}
 
 }
---
+++ Revenda-Veiculo/src/model/dao/ClienteDAO.java
@@ -0,0 +1,43 @@
+package model.dao;
+
+import java.util.ArrayList;
+
+import model.vo.ClienteVO;
+
+public class ClienteDAO {
+
+	public int cadastrarClienteDAO(ClienteVO clienteVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int atualizarClienteDAO(ClienteVO clienteVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int excluirClienteDAO(ClienteVO clienteVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ClienteVO consultarUmClienteDAO(ClienteVO clienteVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<ClienteVO> consultarTodosClientesDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean verficarCliente(String cpf) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean verificarClientePorId(int idCliente) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
