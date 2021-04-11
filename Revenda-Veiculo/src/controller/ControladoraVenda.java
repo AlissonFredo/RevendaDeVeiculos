19102e47d7e41f29f39f9577405ab80271889077
alisson fredo <douglasalissonsf@gmail.com>
2021-04-11 03:10:28 -0300

    create class ControladoraVenda e conclusão da class MenuVenda

---
+++ Revenda-Veiculo/src/controller/ControladoraVenda.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.vo.VendaVO;
+
+public class ControladoraVenda {
+
+	public String cadastrarVendaController(VendaVO vendaVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String atualizarVendaController(VendaVO vendaVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String excluirVendaController(VendaVO vendaVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<VendaVO> consultarTodasVendasController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VendaVO consultarUmaVendaController(VendaVO vendaVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- Revenda-Veiculo/src/view/MenuVenda.java
+++ Revenda-Veiculo/src/view/MenuVenda.java
@@ -1,8 +1,11 @@
 package view;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import controller.ControladoraVenda;
 import model.vo.VendaVO;
 
 public class MenuVenda {
@@ -48,22 +51,92 @@ public class MenuVenda {
 	}
 
 	private void excluirVenda() {
-		// TODO Auto-generated method stub
-		
+		VendaVO vendaVO = new VendaVO();
+		System.out.print("\nDigite o codigo da venda: ");
+		vendaVO.setIdVenda(Integer.parseInt(teclado.nextLine()));
+
+		ControladoraVenda controladoraVenda = new ControladoraVenda();
+		String resultado = controladoraVenda.excluirVendaController(vendaVO);
+		System.out.println(resultado);
 	}
 
 	private void atualizarVenda() {
-		// TODO Auto-generated method stub
-		
+		VendaVO vendaVO = new VendaVO();
+		System.out.print("\nDigite o codigo da venda: ");
+		vendaVO.setIdVenda(Integer.parseInt(teclado.nextLine()));
+		System.out.print("Digite o codigo do cliente: ");
+		vendaVO.setIdCliente(Integer.parseInt(teclado.nextLine()));
+		System.out.print("Digite o codigo do veiculo: ");
+		vendaVO.setIdVeiculo(Integer.parseInt(teclado.nextLine()));
+		System.out.print("Digite o valor da venda: ");
+		vendaVO.setValorVenda(Double.parseDouble(teclado.nextLine()));
+		System.out.print("Digite a data da venda: ");
+		vendaVO.setDataVenda(LocalDate.parse(teclado.nextLine(), dataFormatter));
+
+		ControladoraVenda controladoraVenda = new ControladoraVenda();
+		String resultado = controladoraVenda.atualizarVendaController(vendaVO);
+		System.out.println(resultado);
 	}
 
 	private void consultarVenda() {
-		// TODO Auto-generated method stub
-		
+		int opcao = this.apresentarMenuConsultaVenda();
+		ControladoraVenda controladoraVenda = new ControladoraVenda();
+		while (opcao != OPCAO_MENU_CONSULTA_VENDAS_VOLTAR) {
+			switch (opcao) {
+			case OPCAO_MENU_CONSULTA_TODAS_VENDAS: {
+				opcao = OPCAO_MENU_CONSULTA_VENDAS_VOLTAR;
+				ArrayList<VendaVO> listaVendaVO = controladoraVenda.consultarTodasVendasController();
+				System.out.println("|| Resultado da consulta ||");
+				for (int i = 0; i < listaVendaVO.size(); i++) {
+					System.out.println(listaVendaVO.get(i).toString());
+				}
+				System.out.println();
+				break;
+			}
+			case OPCAO_MENU_CONSULTA_UMA_VENDA: {
+				VendaVO vendaVO = new VendaVO();
+				System.out.print("\nDigite o codigo da venda: ");
+				vendaVO.setIdVenda(Integer.parseInt(teclado.nextLine()));
+
+				VendaVO venda = controladoraVenda.consultarUmaVendaController(vendaVO);
+				System.out.println("|| Resultado da consulta ||");
+				if (venda == null) {
+					System.out.println("Não foi possivel encontrar cliente");
+				} else {
+					System.out.println(venda.toString());
+				}
+			}
+			default: {
+				System.err.println("Opção invalida!");
+				this.apresentarMenuConsultaVenda();
+			}
+			}
+		}
+	}
+
+	private int apresentarMenuConsultaVenda() {
+		System.out.println("\n|| Opções de consulta ||");
+		System.out.println(OPCAO_MENU_CONSULTA_TODAS_VENDAS + " - Consultar todas as vendas");
+		System.out.println(OPCAO_MENU_CONSULTA_UMA_VENDA + " - Consultar uma venda");
+		System.out.println(OPCAO_MENU_CONSULTA_VENDAS_VOLTAR + " - Voltar");
+		System.out.print("Digite a opção:");
+		return Integer.parseInt(teclado.nextLine());
 	}
 
 	private void cadastrarVenda() {
-		VendaVO vendaVO = new VendaVO(); 
+		VendaVO vendaVO = new VendaVO();
+		System.out.print("\nDigite o codigo do cliente: ");
+		vendaVO.setIdCliente(Integer.parseInt(teclado.nextLine()));
+		System.out.print("Digite o codigo do veiculo: ");
+		vendaVO.setIdVeiculo(Integer.parseInt(teclado.nextLine()));
+		System.out.print("Digite o valor da venda: ");
+		vendaVO.setValorVenda(Double.parseDouble(teclado.nextLine()));
+		System.out.print("Digite a data da venda: ");
+		vendaVO.setDataVenda(LocalDate.parse(teclado.nextLine(), dataFormatter));
+
+		ControladoraVenda controladoraVenda = new ControladoraVenda();
+		String resultado = controladoraVenda.cadastrarVendaController(vendaVO);
+		System.out.println(resultado);
 	}
 
 	private int apresentarOpcaoMenuVenda() {
